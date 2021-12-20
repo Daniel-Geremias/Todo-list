dc076912995af9109981017c6666614ebd9cf2af
Daniel Geremias <daniel.vg19@gmail.com>
2021-12-20 16:30:59 -0300

    Organizing stuff

---
+++ js/app.js
@@ -0,0 +1,20 @@
+//Select the Element
+const clear = document.querySelector(".clear");
+const dateElement = document.getElementById("date");
+const list = document.getElementById("list");
+const input = document.getElementById("input");
+
+//Class names
+const CHECK = "fa-check-circle";
+const UNCHECK = "fa-circle-thin";
+const LINE_THROUGH = "lineThrough";
+
+//Show todays date
+
+const options = {weekday:"long", month:"short", day:"numeric"};
+const today = new Date();
+
+dateElement.innerHTML = today.toLocaleDateString("en-US", options);
+
+//add todo function
+function addToDo(todo)
